7f0368a3646e09d1911714f7cfe4c989236ce86b
Acrylic125 <acrylic125email@gmail.com>
2024-07-23 11:56:11 +0800

    conway game of life

---
+++ conway-gol/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const int GRID_X = 64;
+const int GRID_Y = 64;
+
+const int DEAD = 0;
+const int ALIVE = 1;
+
+int main()
+{
+    int grid[GRID_X][GRID_Y];
+    for (int x = 0; x < GRID_X; x++)
+    {
+        // string row = "";
+        // row.
+    }
+    return 0;
+}
